8a337c4db2e2f45565b5524445318853e5273bbc
AdnrewRoni <andrewroni@gmail.com>
2018-10-03 10:44:02 +0300

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -22,6 +22,16 @@ socket.emit('createMessage', {
   text: 'Hi!'
 }, (data) => console.log('Got It', data));
 
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
@@ -30,4 +40,22 @@ jQuery('#message-form').on('submit', function (e) {
     text: jQuery('[name=message]').val()
   }, function () {
   });
-})
+});
+
+let locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+  if(!navigator.geolocation) {
+    return alert('Geolocation not suppoerted by tour browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fertch location.');
+  });
+}
+);
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,5 +9,19 @@ describe('generateMessage', () => {
 
     expect(res).toMatchObject(message);
     expect(typeof res.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    let from = 'Admin';
+    let latitude = 1;
+    let longitude = 1;
+    let url = 'https://www.google.com/maps?q=1,1';
+    let res = generateLocationMessage(from, latitude, longitude);
+
+    expect(res.from).toBe(from);
+    expect(typeof res.createdAt).toBe('number');
+    expect(res.url).toBe(url);
   });
 });
